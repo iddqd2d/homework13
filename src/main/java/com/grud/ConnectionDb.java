9f8fd8e31fa16c9d65062c6258afc64b0c7462ef
iddqd2d <iddqd2d@gmail.com>
2019-04-11 17:23:19 +0300

    add config.properties

--- src/main/java/com/grud/ConnectionDb.java
+++ src/main/java/com/grud/ConnectionDb.java
@@ -1,16 +1,21 @@
 package com.grud;
 
 import lombok.SneakyThrows;
+
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class ConnectionDb {
-    private static final String url = "jdbc:mysql://localhost/freelancers_site?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private static final String user = "root";
-    private static final String password = "";
 
     @SneakyThrows
     public static Connection getConnection() {
-        return DriverManager.getConnection(url, user, password);
+        Properties propertiesDb = new Properties();
+        propertiesDb.load(new FileInputStream("src/main/resources/config.properties"));
+        String host = propertiesDb.getProperty("db.host");
+        String login = propertiesDb.getProperty("db.login");
+        String password = propertiesDb.getProperty("db.password");
+        return DriverManager.getConnection(host, login, password);
     }
 }
